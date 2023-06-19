cf00c8be1584e7db7df1172b026d04d3bc1eb240
raghav-rama <hackerer528@gmail.com>
2023-06-19 11:14:09 +0530

    feat: initialize aggregator account

--- programs/switchboard-init/src/lib.rs
+++ programs/switchboard-init/src/lib.rs
@@ -1,4 +1,11 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::clock;
+use std::convert::TryInto;
+use switchboard_v2::{
+    AggregatorAccountData,
+    SwitchboardDecimal,
+    SWITCHBOARD_PROGRAM_ID,
+};
 
 declare_id!("5F1aAkVdT4M4b2XcBAtuMWKXRM9niTTnqg3TzAWZeANo");
 
@@ -12,11 +19,13 @@ pub mod switchboard_init {
         switchboard.data = 0;
         Ok(())
     }
+
+
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, payer = user, space = 8 + 8)]
+    #[account(init, payer = user, space = 8 + 32 + 8)]
     pub switchboard: Account<'info, Switchboard>,
     #[account(mut)]
     pub user: Signer<'info>,
@@ -27,4 +36,26 @@ pub struct Initialize<'info> {
 pub struct Switchboard {
     pub authority: Pubkey,
     pub data: u64,
+}
+
+#[derive(Accounts)]
+#[instruction(params: ReadResultParams)]
+pub struct ReadResults<'info> {
+    pub aggregator: AccountLoader<'info, AggregatorAccountData>,
+}
+
+#[derive(Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ReadResultParams {
+    pub max_confidence_interval: Option<f64>,
+}
+
+#[error_code]
+#[derive(Eq, PartialEq)]
+pub enum FeedErrorCode {
+    #[msg("Not a valid switchboard account")]
+    InvalidSwitchboardAccount,
+    #[msg("Switchboard feed has not been updated in 5 minutes")]
+    StaleFeed,
+    #[msg("Switchboard feed exceeded confidence interval")]
+    ConfidenceIntervalExceeded
 }
